1360d1f00ca716a617f34b639357ca5f19f744f7
stieglitzz <samanthariegl@gmail.com>
2023-09-22 13:14:45 +1000

    Update fonts, pages, scripts, CSS and templates.
    - updated pageTemplate.html
    - update scriptTemplate.js
    - copied HTML from pageTemplate.html into each page
    - updated linked JS file on each page EXCEPT for stickerMaking101.js
    - updated style.css
    - added fonts to font folder

---
+++ js/diyScreenPrinting.js
@@ -0,0 +1,145 @@
+// Variables
+const publicSpreadsheetUrl = "https://docs.google.com/spreadsheets/d/e/2PACX-1vSdFRuJfA88dCYlH3wmM4QiKAW5gaWDpFQG_x4cwen9j4QzSscebNccC6eYQp2wersIY1VzdxoXS8Vo/pub?output=csv";
+const categoryStartNum = 4;
+const sheetName = "diyScreenPrinting";
+
+// Papaparse
+function init() {
+    Papa.parse(publicSpreadsheetUrl, {
+        download: true,
+        header: true,
+        complete: showInfo,
+    });
+}
+
+console.log(showInfo);
+
+function showInfo(results) {
+    const data = results.data;
+    const checked = "x";
+    const columnArray = Object.keys(data[0]);
+    const columnName = [columnArray.length];
+    for (let j = 0; j < columnArray.length; j++) {
+        columnName[j] = columnArray[j];
+    }
+
+    // Create sorting buttons
+    for (let j = categoryStartNum; j < columnArray.length; j++) {
+        addButton(columnName[j]);
+    }
+
+    for (let i = categoryStartNum; i < columnArray.length; i++) {
+        for (let j = 0; j < data.length; j++) {
+            var category = columnArray[i];
+            var columnData = data[j][category];
+
+            if (columnData == checked) {
+                addElement(columnName[i], data[j][columnName[0]], data[j][columnName[1]], data[j][columnName[2]], data[j][columnName[3]]);
+            }
+        }
+    }
+    // Click the "All" button programmatically
+    document.querySelector(".filterButton").click();
+}
+
+function addButton(columnName) {
+    const newButton = document.createElement("BUTTON");
+    const newButtonContent = document.createTextNode(columnName);
+
+    newButton.appendChild(newButtonContent);
+    newButton.className = "filterButton";
+    newButton.addEventListener("click", function () {
+        filterSelection(columnName);
+        filterItemOff(); // turns off all active buttons
+        newButton.classList.add("active"); // turn this button on
+    });
+    document.getElementById("filterButtonWrap").appendChild(newButton);
+}
+
+console.log(addButton);
+
+function filterItemOff() {
+    let filterItemClassArray = document.getElementsByClassName("filterButton");
+    for (let i = 0; i < filterItemClassArray.length; i++) {
+        if (filterItemClassArray[i].classList.contains("active")) {
+            filterItemClassArray[i].classList.remove("active");
+        }
+    }
+}
+
+function addElement(columnName, title, url, description, icon) {
+    // class names
+    const hashtagOne = ["item"];
+    const hashtagTwo = [columnName];
+    const hashtagArray = hashtagOne.concat(hashtagTwo);
+    const hashtagString = hashtagArray.join(" ");
+    const newItem = document.createElement("a");
+    newItem.className = hashtagString;
+    newItem.href = url;
+
+    // iconwrap and svg
+    const iconWrap = document.createElement("div");
+    iconWrap.className = "itemIconWrap";
+    iconWrap.innerHTML = icon;
+
+    // text wrap
+    const textWrap = document.createElement("div");
+    textWrap.className = "itemTextWrap";
+
+    // Create the title element
+    const itemTitle = document.createElement("h3");
+    itemTitle.innerText = `${title}`;
+    textWrap.appendChild(itemTitle);
+
+    // Create the description element if description exists
+    if (description) {
+        const itemDescription = document.createElement("p");
+        itemDescription.innerText = `${description}`;
+        textWrap.appendChild(itemDescription);
+    }
+
+    // Append the icon and content div to the item div
+    newItem.appendChild(iconWrap);
+    newItem.appendChild(textWrap);
+
+    document.getElementsByTagName("main")[0].appendChild(newItem);
+
+    // Shuffle the order of the items within the main element
+    const mainElement = document.getElementsByTagName("main")[0];
+    mainElement.insertBefore(newItem, mainElement.children[Math.floor(Math.random() * mainElement.children.length)]);
+}
+
+window.addEventListener("DOMContentLoaded", init);
+
+// filter script
+function filterSelection(c) {
+    var x, i;
+    x = document.getElementsByClassName("item");
+    for (i = 0; i < x.length; i++) {
+        w3RemoveClass(x[i], "show");
+        if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
+    }
+}
+
+function w3AddClass(element, name) {
+    var i, arr1, arr2;
+    arr1 = element.className.split(" ");
+    arr2 = name.split(" ");
+    for (i = 0; i < arr2.length; i++) {
+        if (arr1.indexOf(arr2[i]) == -1) {
+            element.className += " " + arr2[i];
+        }
+    }
+}
+
+function w3RemoveClass(element, name) {
+    var i, arr1, arr2;
+    arr1 = element.className.split(" ");
+    arr2 = name.split(" ");
+    for (i = 0; i < arr2.length; i++) {
+        while (arr1.indexOf(arr2[i]) > -1) {
+            arr1.splice(arr1.indexOf(arr2[i]), 1);
+        }
+    }
+    element.className = arr1.join(" ");
+}
--- templates/scriptTemplate.js
+++ templates/scriptTemplate.js
@@ -1,139 +1,145 @@
-// editable info
+// Variables
+const publicSpreadsheetUrl = "❎❎❎";
+const categoryStartNum = 4;
+const sheetName = "❎❎❎";
 
-const publicSpreadsheetUrl =
-  'https://docs.google.com/spreadsheets/d/e/2PACX-1vTSL7xFijPpq7-IseyKEVnE1k0cDW-LHERb0-b5vxT4yOSg3neXgyC5cljBStGvb9kJ3h9QTlWrGGdZ/pub?output=csv'
-
-const categoryStartNum = 3 // let the program know where the categoy begins on the spreadsheet column. Default value is 3.
-const sheetName = 'Sheet1' // this has to match your google doc sheet name
-const punctuation = ',' // this changes the punctuation between the title and the description. In most cases you'd want to use "," or "-" or ":"
-
-// Papa parse
+// Papaparse
 function init() {
-  Papa.parse(publicSpreadsheetUrl, {
-    download: true,
-    header: true,
-    complete: showInfo,
-  })
+    Papa.parse(publicSpreadsheetUrl, {
+        download: true,
+        header: true,
+        complete: showInfo,
+    });
 }
 
+console.log(showInfo);
+
 function showInfo(results) {
-  const data = results.data
-
-  const checked = 'x'
-  const columnArray = Object.keys(data[0])
-
-  const columnName = [columnArray.length]
-
-  for (let j = 0; j < columnArray.length; j++) {
-    columnName[j] = columnArray[j]
-  }
-
-  // create sorting buttons
-  for (let j = categoryStartNum; j < columnArray.length; j++) {
-    addButton(columnName[j])
-  }
-
-  for (let i = categoryStartNum; i < columnArray.length; i++) {
-    for (let j = 0; j < data.length; j++) {
-      var category = columnArray[i]
-      var columnData = data[j][category]
-
-      if (columnData == checked) {
-        addElement(
-          columnName[i],
-          data[j][columnName[0]],
-          data[j][columnName[1]],
-          data[j][columnName[2]],
-        )
-      }
+    const data = results.data;
+    const checked = "x";
+    const columnArray = Object.keys(data[0]);
+    const columnName = [columnArray.length];
+    for (let j = 0; j < columnArray.length; j++) {
+        columnName[j] = columnArray[j];
     }
-  }
-  // alert("Successfully processed!"); // check if data is imported
+
+    // Create sorting buttons
+    for (let j = categoryStartNum; j < columnArray.length; j++) {
+        addButton(columnName[j]);
+    }
+
+    for (let i = categoryStartNum; i < columnArray.length; i++) {
+        for (let j = 0; j < data.length; j++) {
+            var category = columnArray[i];
+            var columnData = data[j][category];
+
+            if (columnData == checked) {
+                addElement(columnName[i], data[j][columnName[0]], data[j][columnName[1]], data[j][columnName[2]], data[j][columnName[3]]);
+            }
+        }
+    }
+    // Click the "All" button programmatically
+    document.querySelector(".filterButton").click();
 }
 
 function addButton(columnName) {
-  const newButton = document.createElement('BUTTON')
-  const newButtonContent = document.createTextNode(columnName)
+    const newButton = document.createElement("BUTTON");
+    const newButtonContent = document.createTextNode(columnName);
 
-  newButton.appendChild(newButtonContent)
-  newButton.className = 'btn'
-  newButton.addEventListener('click', function () {
-    filterSelection(columnName)
-    btnOff() // turns off all active buttons
-    newButton.classList.add('active') // turn this button on
-  })
-  document.getElementById('myBtnContainer').appendChild(newButton)
+    newButton.appendChild(newButtonContent);
+    newButton.className = "filterButton";
+    newButton.addEventListener("click", function () {
+        filterSelection(columnName);
+        filterItemOff(); // turns off all active buttons
+        newButton.classList.add("active"); // turn this button on
+    });
+    document.getElementById("filterButtonWrap").appendChild(newButton);
 }
 
-function btnOff() {
-  let btnClassArray = document.getElementsByClassName('btn')
-  for (let i = 0; i < btnClassArray.length; i++) {
-    if (btnClassArray[i].classList.contains('active')) {
-      btnClassArray[i].classList.remove('active')
+console.log(addButton);
+
+function filterItemOff() {
+    let filterItemClassArray = document.getElementsByClassName("filterButton");
+    for (let i = 0; i < filterItemClassArray.length; i++) {
+        if (filterItemClassArray[i].classList.contains("active")) {
+            filterItemClassArray[i].classList.remove("active");
+        }
     }
-  }
 }
 
-function addElement(columnName, person, url, description) {
-  const hashtag1 = ['filterDiv']
-  const hashtag2 = [columnName]
-  const hashtagArray = hashtag1.concat(hashtag2)
-  const hashtagString = hashtagArray.join(' ')
-  const newDiv = document.createElement('div')
-  newDiv.className = hashtagString
+function addElement(columnName, title, url, description, icon) {
+    // class names
+    const hashtagOne = ["item"];
+    const hashtagTwo = [columnName];
+    const hashtagArray = hashtagOne.concat(hashtagTwo);
+    const hashtagString = hashtagArray.join(" ");
+    const newItem = document.createElement("a");
+    newItem.className = hashtagString;
+    newItem.href = url;
 
-  // place individual link inside individual div
-  for (let i = 0; i < 1; i++) {
-    let link = document.createElement('a')
-    let linkContent = document.createTextNode(person)
-    link.appendChild(linkContent)
-    link.title = person
-    link.href = url
-    link.className = 'itemLink'
+    // iconwrap and svg
+    const iconWrap = document.createElement("div");
+    iconWrap.className = "itemIconWrap";
+    iconWrap.innerHTML = icon;
 
-    let para = document.createElement('p')
-    let paraContent = document.createTextNode(`${punctuation} ${description}`)
-    para.appendChild(paraContent)
-    para.className = 'itemPara'
+    // text wrap
+    const textWrap = document.createElement("div");
+    textWrap.className = "itemTextWrap";
 
-    para.appendChild(link) // put <a> into <p>
-    link.after(paraContent) // put <p> description after <a>
-    newDiv.appendChild(para) // put <p> into newDiv
-  }
-  document.getElementsByClassName('container')[0].appendChild(newDiv)
+    // Create the title element
+    const itemTitle = document.createElement("h3");
+    itemTitle.innerText = `${title}`;
+    textWrap.appendChild(itemTitle);
+
+    // Create the description element if description exists
+    if (description) {
+        const itemDescription = document.createElement("p");
+        itemDescription.innerText = `${description}`;
+        textWrap.appendChild(itemDescription);
+    }
+
+    // Append the icon and content div to the item div
+    newItem.appendChild(iconWrap);
+    newItem.appendChild(textWrap);
+
+    document.getElementsByTagName("main")[0].appendChild(newItem);
+
+    // Shuffle the order of the items within the main element
+    const mainElement = document.getElementsByTagName("main")[0];
+    mainElement.insertBefore(newItem, mainElement.children[Math.floor(Math.random() * mainElement.children.length)]);
 }
 
-window.addEventListener('DOMContentLoaded', init)
+window.addEventListener("DOMContentLoaded", init);
 
 // filter script
 function filterSelection(c) {
-  var x, i
-  x = document.getElementsByClassName('filterDiv')
-  for (i = 0; i < x.length; i++) {
-    w3RemoveClass(x[i], 'show')
-    if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], 'show')
-  }
+    var x, i;
+    x = document.getElementsByClassName("item");
+    for (i = 0; i < x.length; i++) {
+        w3RemoveClass(x[i], "show");
+        if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
+    }
 }
 
 function w3AddClass(element, name) {
-  var i, arr1, arr2
-  arr1 = element.className.split(' ')
-  arr2 = name.split(' ')
-  for (i = 0; i < arr2.length; i++) {
-    if (arr1.indexOf(arr2[i]) == -1) {
-      element.className += ' ' + arr2[i]
+    var i, arr1, arr2;
+    arr1 = element.className.split(" ");
+    arr2 = name.split(" ");
+    for (i = 0; i < arr2.length; i++) {
+        if (arr1.indexOf(arr2[i]) == -1) {
+            element.className += " " + arr2[i];
+        }
     }
-  }
 }
 
 function w3RemoveClass(element, name) {
-  var i, arr1, arr2
-  arr1 = element.className.split(' ')
-  arr2 = name.split(' ')
-  for (i = 0; i < arr2.length; i++) {
-    while (arr1.indexOf(arr2[i]) > -1) {
-      arr1.splice(arr1.indexOf(arr2[i]), 1)
+    var i, arr1, arr2;
+    arr1 = element.className.split(" ");
+    arr2 = name.split(" ");
+    for (i = 0; i < arr2.length; i++) {
+        while (arr1.indexOf(arr2[i]) > -1) {
+            arr1.splice(arr1.indexOf(arr2[i]), 1);
+        }
     }
-  }
-  element.className = arr1.join(' ')
+    element.className = arr1.join(" ");
 }
